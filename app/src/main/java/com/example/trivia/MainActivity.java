673a537c1874862dbac4193018bdd4306f2af46e
eherzstein <eherzstein@tfs.ca>
2021-02-02 08:32:19 -0500

    Generating question and answers

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -14,11 +14,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
 import com.android.volley.Request;
@@ -27,20 +22,22 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
-   RadioButton choice1, choice2, choice3;
+   RadioButton choice1, choice2, choice3, choice4;
    RadioGroup radioGroup;
+   Button generate, verify;
+   TextView question;
 
    String category = "";
    String difficulty = "";
-   String [] choices = new String [] {"0","1","2","correct_answer"};
+   String [] choices = new String [4];
 
-	public void shuffleChoices(String choices[])
+	public void shuffleChoices(String choices[], RadioGroup radioGroup)
     {
        // choices = new String [] {"0","1","2","correct_answer"};
+
 
         Random rand = new Random();
 
@@ -50,6 +47,36 @@ public class MainActivity extends AppCompatActivity {
             choices[randomSwap] = choices[i];
             choices[i] = temp;
         }
+
+        /*for (int i = 0; i < radioGroup.getChildCount(); i++) {
+            ((RadioButton) radioGroup.getChildAt(i)).setText(String.valueOf(choices[i]));
+        }*/
+
+       // Log.d("tag","hello" + choice4.getText().toString());
+
+    }
+
+    public String correctFormat(String str)
+    {
+       // if(str.contains("&oacute;"))
+            str = str.replace("&oacute;","ó");
+
+      //  if(str.contains("&quot;"))
+        str = str.replace("&quot;","'");
+
+       // if(str.contains("&Eacute;"))
+        str = str.replace("&Eacute;","É");
+
+       // if(str.contains("&#039;"))
+        str = str.replace("&#039;","'");
+
+       // if(str.contains("&amp;"))
+        str = str.replace("&amp;","&");
+
+       // if(str.contains("&shy;"))
+        str = str.replace("&shy;","");
+
+       return str;
     }
 
     @Override
@@ -60,20 +87,102 @@ public class MainActivity extends AppCompatActivity {
         choice2 = findViewById(R.id.choice2);
         choice3 = findViewById(R.id.choice3);
         radioGroup = findViewById(R.id.radioGroup);
+        generate = findViewById(R.id.generate);
+        question = findViewById(R.id.question);
+        verify = findViewById(R.id.verify);
+        afficher();
+
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                afficher();
+            }
+        });
+
+        verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               // verify(verify);
+            }
+        });
     }
 
-    public void afficher()
+    /*public void verify(String correct)
     {
         String url = "https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple";
+
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            shuffleChoices(choices);
 
 
-                           // choice1.setText();
+                            for(int k=0; k<radioGroup.getChildCount(); k++) {
+                                if (((RadioButton) radioGroup.getChildAt(k)).getText().equals(correct))
+                                {
+
+
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Tag","resultat =  "+error);
+            }
+        });
+        // ajouter tous les éléments à la queue
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
+    }*/
+
+    public void afficher()
+    {
+        String url = "https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            int amount = 50;
+
+                            JSONArray array = response.getJSONArray("results");
+
+                            //get random question
+                            Random rand = new Random();
+                            int randomNum = rand.nextInt(amount);
+                            JSONObject object = array.getJSONObject(randomNum);
+
+                            String q = object.getString("question");
+
+                            question.setText(correctFormat(q));
+
+                            JSONArray incorrect_answers = object.getJSONArray("incorrect_answers");
+
+                            String correct = object.getString("correct_answer");
+                            choices[0] = correct;
+
+                            for (int i = 1; i < radioGroup.getChildCount(); i++) {
+                                choices[i] = incorrect_answers.getString(i-1);
+                                choices[i] = correctFormat(choices[i]);
+                            }
+
+                            shuffleChoices(choices, radioGroup);
+
+                          //  Log.d("tag","helloo"+incorrect_answers.getJSONObject(0).toString());
+
+                            for (int j = 0; j < radioGroup.getChildCount(); j++) {
+                                ((RadioButton) radioGroup.getChildAt(j)).setText(choices[j]);
+                            }
+
+
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
